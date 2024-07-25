a36b4139bd04d93d9a1caf0bf500990cb62b0059
Ezeh Cynthia Ifeoma <goldncyndy@gmail.com>
2024-07-25 14:02:27 +0100

    Initial Commit

---
+++ PersonalDiary/PersonalDiary/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalDiary
+//
+//  Created by Cynthia D'Phoenix on 7/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
